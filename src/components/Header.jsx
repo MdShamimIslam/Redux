c3ca4d2712dbd0727acb20c0864fdeec4c9520e2
MdShamimIslam <shamim401897@gmail.com>
2024-09-28 09:14:03 +0600

    explore middlewares,store,etc

---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import tickImage from "../assets/images/double-tick.png";
+import { added, allCompleted, clearCompleted } from "../redux/todos/actions";
+
+export default function Header() {
+  const dispatch = useDispatch();
+  const [input, setInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(added(input));
+    setInput("");
+  };
+
+  const handleComplete = ()=> {
+    dispatch(allCompleted())
+  }
+
+  const handleClear = ()=> {
+    dispatch(clearCompleted())
+  }
+
+  return (
+    <div>
+      <form
+        className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+        onSubmit={handleSubmit}
+      >
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type your todo"
+          className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
+        />
+        <button
+          className="bg-yellow-800 p-2 rounded-lg text-white"
+          type="submit"
+        >
+          Add
+        </button>
+      </form>
+
+      <ul className="flex justify-between my-4 text-xs text-gray-500">
+        <li className="flex items-center space-x-1 cursor-pointer" onClick={handleComplete}>
+          <img className="w-6 h-6" src={tickImage} alt="Complete" />
+          <span>Complete All Tasks</span>
+        </li>
+        <li className="cursor-pointer" onClick={handleClear}>Clear completed</li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/redux/todos/todoReducer.js
@@ -0,0 +1,74 @@
+import {
+  ADDED,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  COLORSELECTED,
+  DELETED,
+  TOGGLED,
+} from "./actionTypes";
+import { initialState } from "./initialState";
+
+const nextTodoId = (todos) => {
+  const maxId = todos.reduce(
+    (accumulatorId, currentTodoId) => Math.max(currentTodoId.id, accumulatorId),
+    -1
+  );
+  return maxId + 1;
+};
+
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDED:
+      return [
+        ...state,
+        {
+          id: nextTodoId(state),
+          text: action.payload,
+          completed: false,
+        },
+      ];
+
+    case ALLCOMPLETED:
+      return state.map((todo) => {
+        return {
+          ...todo,
+          completed: true,
+        };
+      });
+
+    case CLEARCOMPLETED:
+      return state.filter((todo) => !todo.completed);
+
+    case TOGGLED:
+      return state.map((todo) => {
+        if (todo.id !== action.payload) {
+          return todo;
+        }
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      });
+
+    case DELETED:
+      return state.filter((todo) => todo.id !== action.payload);
+
+    case COLORSELECTED: {
+      const { todoId, color } = action.payload;
+      return state.map((todo) => {
+        if (todo.id !== todoId) {
+          return todo;
+        }
+        return {
+          ...todo,
+          color: color,
+        };
+      });
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
